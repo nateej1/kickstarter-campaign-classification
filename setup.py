fa30c342a4c38d17edda72fda56c9675b58a1495
mishaberrien <markishab@gmail.com>
2019-08-20 13:43:45 -0400

    push new structcure to github

--- setup.py
+++ setup.py
@@ -1,16 +1,3 @@
 from setuptools import setup, find_packages
-PACKAGES = find_packages()
 
-opts = dict(name="mypackage",
-            maintainer="Author Name",
-            maintainer_email="maintainer@domain.com",
-            description="Simple example of a Travis setup",
-            long_description="Simple example of a Travis setup",
-            url="http://github.com/jakevdp/travis-python-template",
-            download_url="http://github.com/jakevdp/travis-python-template",
-            license="MIT",
-            packages=PACKAGES)
-
-
-if __name__ == '__main__':
-    setup(**opts)
+setup(name="d00_utils", packages=find_packages())
