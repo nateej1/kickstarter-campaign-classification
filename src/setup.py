10dfd431aefd1f144ad60133996dcae8d5781409
mishaberrien <markishab@gmail.com>
2019-08-20 23:25:54 -0400

    commented out pandas dependency

--- src/setup.py
+++ src/setup.py
@@ -7,13 +7,12 @@ install_requires = [
     'functools'
 ]
 
-tests_require = ['pytest>=4.0.2']
+# tests_require = ['pytest>=4.0.2']
 
 setup(name='src',
       version='0.0.1',
       description='test of travis and pytest',
       author='Misha Berrien',
       author_email='misha.berrien@gmail.com',
-      tests_require=tests_require,
       install_requires=install_requires,
       packages=['d00_utils'])
