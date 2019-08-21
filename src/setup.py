ac7e556a9c7106b725a4d00be060dbeb00794e48
mishaberrien <markishab@gmail.com>
2019-08-20 23:21:26 -0400

    updated setup with setup requires

--- src/setup.py
+++ src/setup.py
@@ -1,8 +1,19 @@
 from setuptools import setup
 
+install_requires = [
+    'pandas>=0.25.0',
+    'numpy>=1.15.4',
+    'glob2>=0.6',
+    'functools'
+]
+
+tests_require = ['pytest>=4.0.2']
+
 setup(name='src',
       version='0.0.1',
       description='test of travis and pytest',
       author='Misha Berrien',
       author_email='misha.berrien@gmail.com',
+      tests_require=tests_require,
+      install_requires=install_requires,
       packages=['d00_utils'])
