4a0a8d02f76dd1a1a6738c4c2d0374beb0d08c6c
mishaberrien <markishab@gmail.com>
2019-08-20 22:54:50 -0400

    updated requirenments file

--- src/d00_utils/test/test_utils.py
+++ src/d00_utils/test/test_utils.py
@@ -3,11 +3,9 @@ import numpy as np
 import glob
 import functools
 import pytest
-import os
-import sys
 
-src_dir = os.path.join(os.getcwd(), '..', '..', 'src')
-sys.path.append(src_dir)
+# src_dir = os.path.join(os.getcwd(), '..', '..', 'src')
+# sys.path.append(src_dir)
 
 from d00_utils.utils import read_multiple_csv_and_concat
 
