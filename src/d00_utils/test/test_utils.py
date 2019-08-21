c42175b764ff56164d0e302107684f8c5b5d81fb
mishaberrien <markishab@gmail.com>
2019-08-20 22:58:31 -0400

    commented out a bunch of code in test file

--- src/d00_utils/test/test_utils.py
+++ src/d00_utils/test/test_utils.py
@@ -1,8 +1,8 @@
-import pandas as pd
-import numpy as np
-import glob
-import functools
-import pytest
+# import pandas as pd
+# import numpy as np
+# import glob
+# import functools
+# import pytest
 
 # src_dir = os.path.join(os.getcwd(), '..', '..', 'src')
 # sys.path.append(src_dir)
