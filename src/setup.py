82026de2f79afc8d4d93ca1075f3c5b5ff67f483
mishaberrien <markishab@gmail.com>
2019-08-20 23:01:02 -0400

    added pandas to setup.py

--- src/setup.py
+++ src/setup.py
@@ -5,4 +5,4 @@ setup(name='src',
       description='test of travis and pytest',
       author='Misha Berrien',
       author_email='misha.berrien@gmail.com',
-      packages=['d00_utils'])
+      packages=['d00_utils', 'pandas'])
